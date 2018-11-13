0efb54cfece3ac03cadd19cae2d514d6c897ee46
Dustin Leer <dustin@wildcanon.com>
2018-11-13 16:40:09 -0500

    Adds home page template with module builder

---
+++ template-home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * The template for the Homepage
+ *
+ * @package Storey
+ * @since 1.0
+ */
+get_header(); ?>
+
+	<?php get_template_part( 'template-parts/module', 'builder' ); ?>
+
+<?php get_footer(); ?>
