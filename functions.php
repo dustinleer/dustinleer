c197e6b7a499b3223b200d3120bc5dc1abab4f1b
Dustin Leer <dustin@wildcanon.com>
2018-11-14 17:49:27 -0500

    Enqueues font families

--- functions.php
+++ functions.php
@@ -113,10 +113,13 @@ add_action( 'widgets_init', 'dustinleer_widgets_init' );
  */
 function dustinleer_scripts() {
 	wp_enqueue_style( 'dustinleer-style', get_template_directory_uri() . '/style.min.css', false );
+	
+	wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,700');
 
 	wp_enqueue_script( 'dustinleer-main-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'dustinleer-vendor-scripts', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '20151215', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
