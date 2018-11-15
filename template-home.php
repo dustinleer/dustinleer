8deed1d06aad334afc88d33dd9432e8042428a9b
Dustin Leer <dustin@wildcanon.com>
2018-11-14 21:38:44 -0500

    Adds wrapper div

--- template-home.php
+++ template-home.php
@@ -7,8 +7,30 @@
  * @package Storey
  * @since 1.0
  */
-get_header(); ?>
+get_header();
 
-	<?php get_template_part( 'template-parts/module', 'builder' ); ?>
+	//ACF Page Builder
+	get_template_part( 'template-parts/module', 'builder' ); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="wrapper">
+
+				<?php
+					while ( have_posts() ) : the_post();
+					
+					get_template_part( 'template-parts/content', 'page' );
+					
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					
+				endwhile; // End of the loop.
+				?>
+
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
