bcbaedaab7526c798720fe5e82685e72d185cc30
Dustin Leer <dustin@wildcanon.com>
2018-11-14 22:41:38 -0500

    Adds hero for posts
    still need to get the correct headers for the blog posts page and single post page

--- template-parts/module-hero.php
+++ template-parts/module-hero.php
@@ -11,49 +11,108 @@
 
 <?php
 
-// check if the flexible content field has rows of data
-if( have_rows( 'page_modules' ) ) {
+if (is_blog()) { 
 
-	 // loop through the rows of data
-	while ( have_rows( 'page_modules' ) ) {
-		the_row();
+	// set the post ID that you want to use and use it in all
+	// of the function calls where it is needed
+	// and make sure it's an integer value
+	$post_id = intval(get_option('page_for_posts'));
 
-		if( get_row_layout() == 'pm_hero' ) {
+	if( have_rows( 'page_modules', $post_id ) ) {
 
-			// vars
-			$img = get_sub_field( 'pm_hero_image' );
-			// $url = $img['url'];
-			// $title = $img['title'];
-			// $alt = $img['alt'];
-			// $caption = $img['caption'];
+		// loop through the rows of data
+		while ( have_rows( 'page_modules', $post_id ) ) {
+			the_row();
 
-			$header = get_sub_field( 'pm_hero_header' );
-			$content = get_sub_field( 'pm_hero_content' );
-			$link = get_sub_field( 'pm_hero_link' );
+			if( get_row_layout() == 'pm_hero' ) {
 
-            echo '<div class="hero-wrapper">';
-                echo '<section class="hero">'; /*style="background-image: url('. $img["url"] .');">';*/
-					
-					echo '<div class="hero-content-wrapper">';
-						echo '<img class="hero-image" src="' . $img['sizes']['large'] . '" alt="' . $img['alt'] . '" />';
+				// vars
+				$img = get_sub_field( 'pm_hero_image', get_option('page_for_posts') );
+				// $url = $img['url'];
+				// $title = $img['title'];
+				// $alt = $img['alt'];
+				// $caption = $img['caption'];
+
+				$header = get_sub_field( 'pm_hero_header', get_option('page_for_posts') );
+				$content = get_sub_field( 'pm_hero_content', get_option('page_for_posts') );
+				$link = get_sub_field( 'pm_hero_link', get_option('page_for_posts') );
+
+				echo '<div class="hero-wrapper">';
+					echo '<section class="hero">'; /*style="background-image: url('. $img["url"] .');">';*/
 						
-						echo '<section class="hero-content">';
-							echo '<h1 class="title">' . $header . '</h1>';
-							echo '<p class="sub-title">' . $content . '</p>';
-							if ( $link ) {
-								echo '<a class="hero-cta cta" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
-							}
-						echo '</section>';
-					echo '</div>';
-					
-				echo '</section>';
-			echo '</div>';
-        }
+						echo '<div class="hero-content-wrapper">';
+							echo '<img class="hero-image" src="' . $img['sizes']['large'] . '" alt="' . $img['alt'] . '" />';
+							
+							echo '<section class="hero-content">';
+								if ( is_single ) {
+									echo '<h1 class="title">' . $header . '</h1>';
+								} else {
+									$title = get_the_title();
+									echo '<h1 class="title">' . $title . '</h1>';
+								}
+								if ( $content ) {
+									echo '<p class="sub-title">' . $content . '</p>';
+								}
+								if ( $link ) {
+									echo '<a class="hero-cta cta" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
+								}
+							echo '</section>';
+						echo '</div>';
+
+					echo '</section>';
+				echo '</div>';
+			}
+
+		}
 
 	}
-
 } else {
+	// check if the flexible content field has rows of data
+	if( have_rows( 'page_modules' ) ) {
 
+		// loop through the rows of data
+		while ( have_rows( 'page_modules' ) ) {
+			the_row();
+
+			if( get_row_layout() == 'pm_hero' ) {
+
+				// vars
+				$img = get_sub_field( 'pm_hero_image' );
+				// $url = $img['url'];
+				// $title = $img['title'];
+				// $alt = $img['alt'];
+				// $caption = $img['caption'];
+
+				$header = get_sub_field( 'pm_hero_header' );
+				$content = get_sub_field( 'pm_hero_content' );
+				$link = get_sub_field( 'pm_hero_link' );
+
+				echo '<div class="hero-wrapper">';
+					echo '<section class="hero">'; /*style="background-image: url('. $img["url"] .');">';*/
+						
+						echo '<div class="hero-content-wrapper">';
+							echo '<img class="hero-image" src="' . $img['sizes']['large'] . '" alt="' . $img['alt'] . '" />';
+							
+							echo '<section class="hero-content">';
+								if ( $header ) {	
+									echo '<h1 class="title">' . $header . '</h1>';
+								}
+								if ( $content ) {
+									echo '<p class="sub-title">' . $content . '</p>';
+								}
+								if ( $link ) {
+									echo '<a class="hero-cta cta" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
+								}
+							echo '</section>';
+						echo '</div>';
+
+					echo '</section>';
+				echo '</div>';
+			}
+
+		}
+
+	}
 }
 
 ?>
