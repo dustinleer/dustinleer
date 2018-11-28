58a7c5295820ad82cd934892c14b39291a51b81f
Dustin Leer <dustin@wildcanon.com>
2018-11-27 20:38:17 -0500

    Adds testimonial templating

---
+++ template-parts/content-testimonial-single.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Dustin_Leer
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+	$img  = get_field( 'headshot' );
+	$img_alt = $img['alt'];
+	$img_url = $img['url'];
+	$excerpt = get_the_excerpt();
+	
+	if ( $img ) {
+		echo '<img class="content-img" src="' . $img_url . '" width="' . $img['1'] . '" height="' . $img['2'] . '" alt="' . $img_alt . '" />';
+	}
+?>
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} else {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		}
+
+		if ( 'post' === get_post_type() ) { ?>
+		<div class="entry-meta">
+			<?php dustinleer_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		} ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+        <?php
+
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'dustinleer' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dustinleer' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php dustinleer_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ template-parts/content-testimonial.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Dustin_Leer
+ */
+
+?>
+
+<?php
+	/* Create an iterator to count the posts */
+	$post_count = 1;
+
+	/* Start the Loop */
+	while ( have_posts() ) : the_post();
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-attribute="<?php echo $post_count; ?>">
+		<?php
+			$img  = get_field( 'headshot' );
+			$img_alt = $img['alt'];
+			$img_url = $img['url'];
+			$excerpt = get_the_excerpt();
+			
+			if ( $img ) {
+				echo '<img src="' . $img_url . '" width="' . $img['1'] . '" height="' . $img['2'] . '" alt="' . $img_alt . '" />';
+			}
+		?>
+
+		<section class="article">
+			<header class="entry-header">
+				<?php
+				if ( is_singular() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
+
+				if ( 'post' === get_post_type() ) { ?>
+				<div class="entry-meta">
+					<?php dustinleer_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php
+				} ?>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<?php
+
+					echo '<p>' . short_excerpt( $excerpt ) . '...</p>';
+					echo '<a class="button" href="' . get_permalink() . '">Read More</a>';
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dustinleer' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php dustinleer_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
+		</section>
+	</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php
+	$post_count++;
+	endwhile;
+?>
