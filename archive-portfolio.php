ad11011689795395474541161bb688abd1eb7d51
Dustin Leer <dustin@wildcanon.com>
2018-11-27 20:42:31 -0500

    Creates custom archives

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -11,12 +11,12 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="wrapper">
+			<!-- <div class="wrapper"> -->
 
 				<?php
 				if ( have_posts() ) : ?>
 
-					<header class="page-header">
+					<header class="visuallyhidden page-header">
 						<?php
 							the_archive_title( '<h1 class="page-title">', '</h1>' );
 							the_archive_description( '<div class="archive-description">', '</div>' );
@@ -43,7 +43,7 @@ get_header(); ?>
 						get_template_part( 'template-parts/content', 'none' );
 						
 					endif; ?>
-			</div>
+			<!-- </div> -->
 			<?php the_posts_navigation(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
