c6a8499d0c6fae7271246d8986306627806ee9d7
Dustin Leer <dustin@wildcanon.com>
2018-11-30 14:25:53 -0500

    Adds custom portfolio templates

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -9,33 +9,33 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area"> 
+        <section class="portfolio-archive-header">
+            <header class="visuallyhidden page-header">
+                <?php
+                    the_archive_title( '<h1 class="page-title">', '</h1>' );
+                    the_archive_description( '<div class="archive-description">', '</div>' );
+                ?>
+            </header><!-- .page-header -->
+            <div>
+                <?php echo get_field( 'portfolio_intro_section', 'option' ); ?>
+            </div>
+        </section>
+
 		<main id="main" class="site-main" role="main">
 			<!-- <div class="wrapper"> -->
 
 				<?php
 				if ( have_posts() ) : ?>
 
-					<header class="visuallyhidden page-header">
-						<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
 
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
-
-
+                    <?php
 						/*
 						* Include the Post-Format-specific template for the content.
 						* If you want to override this in a child theme, then include a file
 						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						*/
-						get_template_part( 'template-parts/content', get_post_format() );
-
-					endwhile;
+						get_template_part( 'template-parts/content-portfolio', get_post_format() );
 
 					
 					else :
