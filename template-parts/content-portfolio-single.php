f2c5fe3096b5c594b781a7501c62ad9f4261f6d3
Dustin Leer <dustin@wildcanon.com>
2018-11-30 14:26:57 -0500

    Adds templating structure for portfolio pages

---
+++ template-parts/content-portfolio-single.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Dustin_Leer
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+	$img  = get_field( 'headshot' );
+	$img_alt = $img['alt'];
+	$img_url = $img['url'];
+	$excerpt = get_the_excerpt();
+	
+	if ( $img ) {
+		echo '<img class="content-img" src="' . $img_url . '" width="' . $img['1'] . '" height="' . $img['2'] . '" alt="' . $img_alt . '" />';
+	}
+?>
+
+	<div class="entry-content">
+        <?php
+
+            $services_title = get_field( 'services_title' );
+            $services = get_field( 'services' );
+            
+            echo '<div>';
+                echo '<h3 class="services">' . $services_title . '</h3>';
+                echo '<p>';
+                    if( count($services)){
+                        foreach($services as $s=>$service){
+                            if($s) echo ', ';
+                            echo $service;
+                        }
+                    }
+                echo '</p>';
+            echo '</div>';
+
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'dustinleer' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dustinleer' ),
+				'after'  => '</div>',
+			) );
+		?>
+    </div><!-- .entry-content -->
+    
+    <?php
+        $images = get_field( 'image_gallery' );
+        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+        if( $images ) {
+            echo '<div class="gallery">';
+                echo '<ul>';
+                    foreach( $images as $image ) {
+                        echo '<li>';
+                            echo '<a data-fancybox="gallery" href="'. $image['url'] .'">';
+                            // echo wp_get_attachment_image( $image['ID'], $size );
+                                echo '<img src="' . $image['sizes']['large'] . '" alt="' . $image['alt'] . '" />';
+                            echo '</a>';
+                        echo '</li>';
+                    }
+                echo '</ul>';
+            echo '</div>';
+        }
+
+    ?>
+
+	<footer class="entry-footer">
+		<?php dustinleer_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
