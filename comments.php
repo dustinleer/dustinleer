c795b88b092ffd59d3d4528505d29f7dad45f75d
Dustin Leer <dustin@wildcanon.com>
2018-12-10 13:55:42 -0500

    Customizes comments section

--- comments.php
+++ comments.php
@@ -31,13 +31,13 @@ if ( post_password_required() ) {
 				if ( 1 === $comment_count ) {
 					printf(
 						/* translators: 1: title. */
-						esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'dustinleer' ),
+						esc_html_e( '1 comment on &ldquo;%1$s&rdquo;', 'dustinleer' ),
 						'<span>' . get_the_title() . '</span>'
 					);
 				} else {
 					printf( // WPCS: XSS OK.
 						/* translators: 1: comment count number, 2: title. */
-						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'dustinleer' ) ),
+						esc_html( _nx( '%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'dustinleer' ) ),
 						number_format_i18n( $comment_count ),
 						'<span>' . get_the_title() . '</span>'
 					);
@@ -62,6 +62,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'avatar_size' => 60,
 				) );
 			?>
 		</ol><!-- .comment-list -->
