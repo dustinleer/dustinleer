cce778c1bf839497cda6a64d0023c7a395885dcb
Dustin Leer <dustin@wildcanon.com>
2018-12-10 14:04:56 -0500

    Changes content template part to posts

--- search.php
+++ search.php
@@ -33,19 +33,19 @@ get_header(); ?>
 						 * If you want to overload this in a child theme then include a file
 						 * called content-search.php and that will be used instead.
 						 */
-						get_template_part( 'template-parts/content', 'search' );
+						get_template_part( 'template-parts/content', 'posts' );
 
 					endwhile;
 
-					the_posts_navigation();
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif; ?>
+					
+					else :
+						
+						get_template_part( 'template-parts/content', 'none' );
+						
+					endif; ?>
 
 			</div>
+			<?php the_posts_navigation(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
