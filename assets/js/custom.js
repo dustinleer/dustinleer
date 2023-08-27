754d4135b9d5b2256ca4413b237c773a28a64815
Dustin <dustin@inverseparadox.net>
2023-08-26 23:04:42 -0400

    Adds assets folder

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * File main.js.
+ *
+ *
+ */
+jQuery(document).ready(function ($) {
+  $('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+    // "zoom",
+    //"share",
+    // "slideShow",
+    //"fullScreen",
+    //"download",
+    // "thumbs",
+    "close"]
+  });
+});
---
+++ assets/js/custom/main.js
@@ -0,0 +1,19 @@
+/**
+ * File main.js.
+ *
+ *
+ */
+jQuery(document).ready(function ($) {
+    $('[data-fancybox="gallery"]').fancybox({
+        buttons: [
+            // "zoom",
+            //"share",
+            // "slideShow",
+            //"fullScreen",
+            //"download",
+            // "thumbs",
+            "close"
+        ],
+    });
+});
+
